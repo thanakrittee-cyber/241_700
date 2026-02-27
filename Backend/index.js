aeed58fb5ae24c15b6f3799d5ae18f2c8ec25f19
Thanakrit <thanakrit.tee@ku.th>
2026-02-27 13:05:48 +0700

    await asycn

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,92 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.json());
 
-let users = [];
-let counter = 1;
-
-
-app.get('/users',(req, res) => {
-    res.json(users);
-});
-
-app.post('/user', (req, res) => {
-    let user = req.body;
-    user.id=counter
-    counter+=1;
-
-    users.push(user);
-    res.json({
-    message: 'User added successful',
-    user: user
+let conn = null;
+const initMySQL = async () => {
+    conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8700
     });
+    console.log('Connected to MySQL database');
+}
+
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results)
+})
+
+
+app.post('/users', async (req, res) => {
+    try {
+        let user = req.body;
+        const results = await conn.query('INSERT INTO users SET ?', user);
+        console.log('results:', results);
+        res.json({
+            message: 'User added successfully',
+            data: results[0]
+        });
+    } catch (error) {
+        console.error('Error inserting user:', error);
+        res.status(500).json({ message: 'Error adding user' });
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        const results = await conn.query('SELECT * FROM users WHERE id = ?', id);
+        if(results[0].lenght == 0){
+            throw{statusCode:404,message:'User not found'};
+        }
+        res.json(results[0][0]);
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message: error.message || 'Error fetching user'
+        });
+    }
+})
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        let updatedUser = req.body;
+        const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updatedUser, id]);
+        res.json({
+            message: 'User updated successfully',
+            data: results[0]
+        });
+    } catch (error) {
+        console.log('Error updating user:', error);
+        res.status(500).json({ message: 'Error updating user' });
+    }
 });
 
-app.patch('/user/:id', (req,res)=>{
+//path: = DELETE /users/:id for delete user by id
+app.delete('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        const results = await conn.query('DELETE FROM users WHERE id = ?', id);
+        res.json({
+            message: 'User deleted successfully',
+            data: results[0]
+        });
+    } catch (error) {
+        console.log('Error deleting user:', error);
+        res.status(500).json({ message: 'Error deleting user' });
+    }
+});
+
+app.patch('/user/:id', (req, res) => {
     let id = req.params.id;
     let updateUser = req.body;
 
@@ -36,10 +97,10 @@ app.patch('/user/:id', (req,res)=>{
     users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname;
     users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname;
 
-    if(updateUser.firstname){
+    if (updateUser.firstname) {
         users[selectedIndex].firstname = updateUser.firstname;
     }
-    if(updateUser.lastname){
+    if (updateUser.lastname) {
         users[selectedIndex].lastname = updateUser.lastname;
     }
 
@@ -50,19 +111,4 @@ app.patch('/user/:id', (req,res)=>{
             indexUpdate: selectedIndex
         }
     });
-});
-    //ส่ง users ที่อัพเดตแล้วกลับไป
-app.delete('/users/:id',(req, res) => {
-    let id = req.params.id;
-    //หา index จาก id ที่ต้องการลบ
-    let selectedIndex = users.findIndex(user => user.id == id);
-    users.splice(selectedIndex, 1);
-    //ลบ user ออกจาก users
-    res.json({
-            message: 'User deleted successful',
-            indexDelete: selectedIndex
-    });
-});
-app.listen(port, ()=> {
-    console.log(`Server is running on http://localhost:${port}`);
 });
